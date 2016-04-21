04556ee14db11de9e42487c4032e179b63d700b8
Ondřej Čertík <ondrej.certik@gmail.com>
2016-04-21 10:41:30 -0600

    Remove affiliation for now to get authors working

--- authors/list_latex.py
+++ authors/list_latex.py
@@ -9,5 +9,5 @@ author_list = load(open("authors.json"))
 with open("../authors.tex", "wb") as f:
     for author in author_list:
         f.write(("\\author{%s}\n" % author["name"]).encode("utf-8"))
-        f.write(("\\affil{%s}\n" % author["institution"]).encode("utf-8"))
+#        f.write(("\\affil{%s}\n" % author["institution"]).encode("utf-8"))
         f.write("\n".encode("utf-8"))

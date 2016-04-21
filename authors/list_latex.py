f2716be4b4359ddfc6b346803d8b79986562e918
Ondřej Čertík <ondrej.certik@gmail.com>
2016-04-21 11:00:41 -0600

    Improve the generated author list

--- authors/list_latex.py
+++ authors/list_latex.py
@@ -7,7 +7,11 @@ from json import load
 author_list = load(open("authors.json"))
 
 with open("../authors.tex", "wb") as f:
-    for author in author_list:
-        f.write(("\\author{%s}\n" % author["name"]).encode("utf-8"))
-#        f.write(("\\affil{%s}\n" % author["institution"]).encode("utf-8"))
-        f.write("\n".encode("utf-8"))
+    f.write("\\author{%\n")
+    for n, author in enumerate(author_list):
+        f.write(("%s%%\n" % author["name"]).encode("utf-8"))
+        f.write(("\\thanks{%s (\\email{%s}).}\n" \
+                % (author["institution"], author["email"])).encode("utf-8"))
+        if n < len(author_list) - 1:
+            f.write("\\and\n".encode("utf-8"))
+    f.write("}\n")

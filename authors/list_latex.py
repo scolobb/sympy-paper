22e7d37b51049bd96f0db83816d84372a059a604
Aaron Meurer <asmeurer@gmail.com>
2016-04-21 13:08:06 -0400

    Fix the list_latex.py script to work in Python 2 and 3

--- authors/list_latex.py
+++ authors/list_latex.py
@@ -3,15 +3,16 @@
 from __future__ import print_function
 
 from json import load
+from io import open
 
 author_list = load(open("authors.json"))
 
-with open("../authors.tex", "wb") as f:
-    f.write("\\author{%\n")
+with open("../authors.tex", "w") as f:
+    f.write(u"\\author{%\n")
     for n, author in enumerate(author_list):
-        f.write(("%s%%\n" % author["name"]).encode("utf-8"))
-        f.write(("\\thanks{%s (\\email{%s}).}\n" \
-                % (author["institution"], author["email"])).encode("utf-8"))
+        f.write((u"%s%%\n" % author["name"]))
+        f.write(u"\\thanks{%s (\\email{%s}).}\n" \
+                % (author["institution"], author["email"]))
         if n < len(author_list) - 1:
-            f.write("\\and\n".encode("utf-8"))
-    f.write("}\n")
+            f.write(u"\\and\n")
+    f.write(u"}\n")

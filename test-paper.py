3452bb7bda2abc5f0fb3008b4fecb0067f7bae91
Isuru Fernando <isuruf@gmail.com>
2016-05-15 20:49:45 +0530

    Use SymPy's doctesting

--- test-paper.py
+++ test-paper.py
@@ -1,4 +1,8 @@
-import doctest
+from sympy.utilities.runtests import SymPyDocTestFinder, pdoctest, SymPyOutputChecker, SymPyDocTestRunner
+from sympy.core.compatibility import StringIO
+from sympy import pprint_use_unicode, init_printing
+import __future__
+import sys
 
 files = ["introduction.tex",
          "architecture.tex",
@@ -9,20 +13,77 @@ files = ["introduction.tex",
          "printers.tex",
          "solvers.tex",
          "matrices.tex",
-         "domain_specific.tex"
+         "domain_specific.tex",
+
+         "supplement.tex",
+         "gruntz.tex",
+         "series.tex",
+         "logic.tex",
+         "diophantine.tex",
+         "sets.tex",
+         "gamma.tex",
+         "live.tex",
+         "comparison_with_mma.tex",
+         "other_projects_that_use_sympy.tex",
+         "tensors.tex"
         ]
 
 full_text = ""
 
-output_file = "test-paper.txt"
+output_file = "test_full_paper.py"
+
+begin = "\\begin{verbatim}"
+end = "\\end{verbatim}"
+skip = "%no-doctest\n"
 
 for file in files:
     with open(file, 'r') as f:
-        full_text += "\n" + f.read()
+        s = f.read()
+        st = s.find(begin)
+        while st != -1:
+            if not (st >= len(skip)) or s[st - len(skip) : st] != skip:
+                full_text += s[st + len(begin) : s.find(end, st)]
+            st = s.find(begin, st+ len(begin))
 
 full_text = full_text.replace(r'\end{verbatim}', '')
 
 with open(output_file, "w") as f:
-    f.write(full_text)
+    f.write("'''\n %s \n'''" % full_text)
 
-doctest.testfile(output_file, optionflags=doctest.NORMALIZE_WHITESPACE)
+# force pprint to be in ascii mode in doctests
+pprint_use_unicode(False)
+
+# hook our nice, hash-stable strprinter
+init_printing(pretty_print=False)
+
+import test_full_paper
+
+# find the doctest
+module = pdoctest._normalize_module(test_full_paper)
+tests = SymPyDocTestFinder().find(module)
+test = tests[0]
+
+runner = SymPyDocTestRunner(optionflags=pdoctest.ELLIPSIS |
+        pdoctest.NORMALIZE_WHITESPACE |
+        pdoctest.IGNORE_EXCEPTION_DETAIL)
+runner._checker = SymPyOutputChecker()
+
+old = sys.stdout
+new = StringIO()
+sys.stdout = new
+
+future_flags = __future__.division.compiler_flag | __future__.print_function.compiler_flag
+
+try:
+    f, t = runner.run(test, compileflags=future_flags,
+                      out=new.write, clear_globs=False)
+except KeyboardInterrupt:
+    raise
+finally:
+    sys.stdout = old
+
+if f > 0:
+    print(new.getvalue())
+    exit(1)
+else:
+    exit(0)
